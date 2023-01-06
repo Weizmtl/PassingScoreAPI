3d83e6872c8d9da8fc38bac136875a12a7a5bee2
LeahFu <leahfu818@gmail.com>
2023-01-06 11:54:33 -0500

    second modify --Lei

--- src/main/java/ca/vanier/group/PassingScore.java
+++ src/main/java/ca/vanier/group/PassingScore.java
@@ -5,6 +5,7 @@ package ca.vanier.group;
  * @description
  * @create 2023-01-05-20:31
  * This is a new message--Lei
+ * second -Lei
  */
 public class PassingScore {
 	public boolean isPassed(int score) {
